22ddb6c2b486ed208a679b4b8c66a8b22c668e4c
abhinaba900 <abhinabajana900@gmail.com>
2024-02-03 16:03:06 +0530

    link completed

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const authMiddleware = require("./Middleware/auth.middleware");
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:3000"],
     credentials: true,
   })
 );
