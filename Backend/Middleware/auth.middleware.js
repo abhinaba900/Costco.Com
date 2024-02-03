d009ec6207e95628915117c70d662342d23577f5
abhinaba900 <abhinabajana900@gmail.com>
2024-02-03 16:27:31 +0530

    auth middle ware fixed

--- Backend/Middleware/auth.middleware.js
+++ Backend/Middleware/auth.middleware.js
@@ -5,32 +5,29 @@ require("dotenv").config();
 async function authMiddleware(req, res, next) {
   try {
     const { authToken, refreshToken } = req.cookies;
-     console.log(authToken,refreshToken);
+    console.log(authToken, refreshToken);
     // Check if both tokens are missing
     if (!authToken && !refreshToken) {
       return res.status(403).json({
         message:
           "Unauthorized: No authentication token provided. Please log in.",
       });
+    } else {
+      jwt.verify(authToken, process.env.AUTH_KEY, (err, data) => {
+        if (data) {
+          // If authToken is not valid, check refreshToken
+          next();
+        } else {
+          jwt.verify(refreshToken, process.env.REFRESH_KEY, (err, data) => {
+            if (data) {
+              next();
+            } else {
+              res.status(403).send("Not authorized. Please login again.");
+            }
+          });
+        }
+      });
     }
-
-    else{
-       jwt.verify(authToken, process.env.AUTH_KEY, (err, data) => {
-         if (err || !data) {
-           // If authToken is not valid, check refreshToken
-           jwt.verify(refreshToken, process.env.REFRESH_KEY, (err, data) => {
-             if (err || !data) {
-               res.status(403).send("Not authorized. Please login again.");
-             } else {
-               next();
-             }
-           });
-         } else {
-           next(); // authToken is valid
-         }
-       });
-    }
-   
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
